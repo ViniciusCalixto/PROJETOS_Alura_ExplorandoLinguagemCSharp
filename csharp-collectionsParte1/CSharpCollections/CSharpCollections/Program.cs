fd12b188b7bcae92662b20116650974971bfb355
Vinicius.Calixto <vinicius.calixto@iob.com.br>
2023-07-13 08:14:56 -0300

    salvando estudos Listas

---
+++ csharp-collectionsParte1/CSharpCollections/CSharpCollections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            /*string[] aulas = new string[]
+            {
+                aulaIntro,
+                aulaModelando,
+                aulaSets
+            };*/
+
+
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+
+            Console.WriteLine(aulas);
+            
+            Imprimir(aulas);
+
+           // Console.WriteLine("Tecle enter para fechar ...");
+           // Console.ReadLine();
+        }
+        private static void Imprimir(string[] aulas)
+        {
+            for (int i = 0;i < aulas.Length; i++)
+            {
+                Console.WriteLine(aulas[i]);
+            }
+        }
+    }
+}
---
+++ csharp-collectionsParte1/CSharpCollections/Listas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução às Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            // List<string> aulas = new List<string>
+            // {
+            //aulaIntro,
+            //aulaModelando,
+            //aulaSets
+            //
+            //};
+            List<string> aulas = new List<string>();
+
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+
+            
+            Imprimir(aulas);
+
+            Console.WriteLine("primeira: " + aulas.First());
+            Console.WriteLine(aulas.Last());
+
+
+            Console.WriteLine("busca primeira ocorrencia: " + aulas.First(aula =>  aula.Contains("Trabalhando")));
+
+            Console.WriteLine("busca ultima ocorrencia: " + aulas.Last(aula => aula.Contains("Trabalhando")));
+
+            Console.WriteLine("busca ultima ocorrencia: " + aulas.LastOrDefault(aula => aula.Contains("Conclusão")));
+
+            aulas.Reverse();
+            Imprimir(aulas);
+
+            aulas.RemoveAt(aulas.Count - 1);
+            Imprimir(aulas);
+
+            aulas.Add("COnclusão");
+            Imprimir(aulas);
+
+            aulas.Sort();
+            Imprimir(aulas);
+
+            List<string> copia = aulas.GetRange(aulas.Count - 2, 2);
+            Imprimir(copia);
+
+            List<string> clone = new List<string>(aulas);
+            Imprimir(clone);
+            
+            clone.RemoveRange(clone.Count - 2, 2);
+            Imprimir(clone);
+
+            
+
+
+        }
+        private static void Imprimir(List<string> aulas)
+        {
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+           
+            for (int i = 0; i < aulas.Count; i++)
+            {
+                Console.WriteLine(aulas[i]);
+                Console.WriteLine("teste com enter" + aulas[i] + Console.Eniroument.NewLine());
+                
+            }
+
+            aulas.ForEach(aula => 
+            {
+                Console.WriteLine(aula);
+
+            });
+        }
+    }
+}
