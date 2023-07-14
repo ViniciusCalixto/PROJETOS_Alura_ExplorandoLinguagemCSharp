ece78588ab13bad758330e44e001374bee7191c1
Vinicius.Calixto <vinicius.calixto@iob.com.br>
2023-07-13 22:17:55 -0300

    Finalizando curso parte 1 coleções

---
+++ csharp-collection/A52Stack/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A52Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("youtube.com.br");
+            navegador.NavegarPara("Alura.com.br");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Anterior();
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class Navegador
+    {
+        private string atual = "vazia";
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+
+        public Navegador()
+        {
+            Console.WriteLine("Pagina atual: " + atual);
+        }
+
+        internal void Anterior()
+        {
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+
+                atual = historicoAnterior.Pop();
+                Console.WriteLine("Pagina atual: " + atual);
+            }
+            
+        }
+
+        internal void NavegarPara(string url)
+        {
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine("Pagina atual: " + atual);
+
+        }
+
+        internal void Proximo()
+        {
+            if (historicoProximo.Any())
+            {
+                historicoAnterior.Push(atual);
+
+                atual = historicoProximo.Pop();
+                Console.WriteLine("Pagina atual: " + atual);
+            }
+        }
+    }
+}
---
+++ csharp-collection/A53Queue/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A53Queue
+{
+    class Program
+    {
+        static Queue<string> pedagio = new Queue<string>();
+        static void Main(string[] args)
+        {
+
+            Enfileirar("van");
+            Enfileirar("kombi");
+            Enfileirar("guincho");
+            Enfileirar("pichup");
+            
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();            
+            Desenfileirar();
+            Desenfileirar();
+
+            Console.ReadLine();
+        }
+
+        private static void Desenfileirar()
+        {
+            if (pedagio.Any())
+            {
+                if (pedagio.Peek() == "guincho")
+                {
+                    Console.WriteLine("guincho está fazendo o pagamento");
+                }
+                string veiculo = pedagio.Dequeue();
+                Console.WriteLine($"saiu da fila: {veiculo}");
+                imprimirFila();
+            }
+           
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            Console.WriteLine($"entoru na fila: {veiculo}");
+
+            pedagio.Enqueue(veiculo);
+            imprimirFila();
+        }
+
+        private static void imprimirFila()
+        {
+            Console.WriteLine("FILA:");
+
+            foreach (var v in pedagio)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
